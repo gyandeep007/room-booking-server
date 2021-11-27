366e3617c1d5d49978e3cf3c5008e7afb75fe08a
gyandeep <gyandeepsharma007@gmail.com>
2021-11-27 15:26:10 +0530

    base application

---
+++ src/test/java/com/example/roombookingserver/RoomBookingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roombookingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
